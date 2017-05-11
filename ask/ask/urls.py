36bc66cf96063ff88f4d8a64f5053f85de4d787f
Nikolay Yakubitskiy <reddaemon@Reddaemon.local>
2017-05-12 01:41:49 +0300

    db models

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', include('qa.urls')),
+    url(r'^', include('qa.urls')),
     url(r'^login/',include('qa.urls')),
     url(r'^signup/', include('qa.urls')),
     url(r'^question/', include('qa.urls')),
--- ask/qa/models.py
+++ ask/qa/models.py
@@ -3,4 +3,31 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
+class QuestionManager(models.Manager):
+	def new():
+		pass
+	def popular():
+		pass
+
+class Question(models.Model):
+	objects = QuestionManager()
+	title = models.CharField(max_length=255)
+	text = models.TextField()
+	added_at = models.DateTimeField(auto_now_add=True)
+	rating = models.IntegerField()
+	author = models.OneToOneField(User, on_delete=models.CASCADE)
+	like = models.ManyToManyField(User, related_name='question_like_user')
+
+
+class Answer(models.Model):
+	text = models.TextField()
+	added_at = models.DateTimeField(auto_now_add=True)
+	question = models.ForeignKey(Question, on_delete=models.CASCADE)
+	author = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+		
+
