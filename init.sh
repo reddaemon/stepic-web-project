d4b44f05d3fc439de5d8ffbb30c611fbc693d279
Nikolay Yakubitskiy <reddaemon@Reddaemon.local>
2017-05-17 19:33:12 +0300

    update init.sh

--- init.sh
+++ init.sh
@@ -4,3 +4,6 @@ sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello
 sudo ln -s /home/box/web/etc/qa.py /etc/gunicorn.d/qa
 sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start
+mysql -uroot -e "create database qa"
+mysql -uroot -e "create user qa identified by '123456'";
+mysql -uroot -e "grant all on qa.* to qa identified by '123456'";
